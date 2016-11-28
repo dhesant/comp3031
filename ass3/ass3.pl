0837104b1c5fa83f58c090231e5894283e994e59
Dhesant Nakka <dhesant@gmail.com>
2016-11-28 17:45:20 +0800

    ass3: finished q4 and q5

--- ass3/ass3.pl
+++ ass3/ass3.pl
@@ -1,13 +1,14 @@
-% Problem 1
+/* Problem 1 */
 
 dot(L1,L2,X) :-
 	length(L1, Length1),
 	length(L2, Length2),
-	Length1 =\= 0,
-	Length1 =:= Length2,
-	dotHelper(L1,L2,0,X).
+	Length1 =\= 0,		% Ensure L1 has some length
+	Length1 =:= Length2,	% Ensure L1 is the same length as L2
+	dotHelper(L1,L2,0,X).   % Call dotHelper to recursively calculate the dot product
 
-dotHelper(L1,L2,Prev,X) :-
+% Helper function to recursively calculate dot product
+dotHelper(L1,L2,Prev,X) :-      
 	[Head1|Tail1] = L1,
 	[Head2|Tail2] = L2,
 	Current is Prev + (Head1 * Head2),
@@ -15,3 +16,25 @@ dotHelper(L1,L2,Prev,X) :-
 
 dotHelper([],[],Prev,X) :-
 	X = Prev.
+
+/* Problem 2 */
+
+
+/* Problem 3 */
+
+/* The database of adj facts */
+adj([a, [b,c,d]]).
+adj([b, [d,e]]).
+adj([c, []]).
+adj([e, [a]]).
+adj([d, [b]]).
+
+/* Problem 4 */
+degree(V, D) :-
+	adj([V, AdjV]),
+	length(AdjV, D).
+
+/* Problem 5 */
+edge(V1, V2) :-
+	adj([V1, AdjV1]),
+	member(V2, AdjV1).
